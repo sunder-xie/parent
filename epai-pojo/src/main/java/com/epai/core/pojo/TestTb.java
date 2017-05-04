3ef65a5af33d6225381dba7dc63b23e5ad4316a2
hanjunrong <490150385@qq.com>
2017-05-04 12:57:16 +0800

    2017年5月4日12:56:20 pojo类加入

---
+++ epai-pojo/src/main/java/com/epai/core/pojo/TestTb.java
@@ -0,0 +1,53 @@
+package com.epai.core.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class TestTb implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//主键
+	private Integer id;
+	
+	private String name;
+	
+	private Date birthday;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	@Override
+	public String toString() {
+		return "TestTb [id=" + id + ", name=" + name + ", birthday=" + birthday + "]";
+	}
+	
+	
+	
+	
+}
